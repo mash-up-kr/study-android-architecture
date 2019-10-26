f75c498c66cb068e84e31d973d1a341b399c31e9
myung6024 <myung6024@gmail.com>
2019-10-27 00:53:06 +0900

    (#1) Navigation 적용

---
+++ myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.runeanim.mytoyproject.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.runeanim.mytoyproject.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
