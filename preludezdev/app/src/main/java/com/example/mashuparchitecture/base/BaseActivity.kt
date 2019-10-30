6c3b138d83d64e4c4f65189bf0614d3b1854f14b
preludezdev <preludez.dev@gmail.com>
2019-10-31 04:50:36 +0900

    HW-2 BaseActivity 구현 2

---
+++ preludezdev/app/src/main/java/com/example/mashuparchitecture/base/BaseActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mashuparchitecture.base
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+
+        setOrientationToPortrait()
+    }
+
+    private fun setOrientationToPortrait() {
+        try {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        } catch (e: IllegalStateException) {
+            //Android O 에서만 투명 Activity 에서
+            //Only fullscreen opaque activities can request orientation 에러 발생
+        }
+    }
+
+}
