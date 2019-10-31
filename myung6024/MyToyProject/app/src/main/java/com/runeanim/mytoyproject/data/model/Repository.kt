5e20134420ab3ed8ad324944e8e64fcd4e23af2c
myung jun Hyun <myung@vinulabs.com>
2019-10-31 14:20:39 +0900

    (#1) search 화면 및 기능 구현

--- myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/data/model/Repository.kt
+++ myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/data/model/Repository.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Repository(
     @SerializedName("id") val id: Long,
     @SerializedName("name") val name: String,
+    @SerializedName("full_name") val fullName: String,
     @SerializedName("owner") val owner: Owner,
     @SerializedName("description") val description: String?,
     @SerializedName("fork") val fork: Boolean,
--- myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/search/SearchFragment.kt
+++ myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/search/SearchFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.runeanim.mytoyproject.R
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.runeanim.mytoyproject.data.Result.Success
+import com.runeanim.mytoyproject.data.model.Repository
+import com.runeanim.mytoyproject.databinding.SearchFragmentBinding
 import com.runeanim.mytoyproject.domain.SearchRepositoriesUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,23 +20,39 @@ import org.koin.android.ext.android.inject
 class SearchFragment : Fragment() {
 
     private val searchRepositoriesUseCase: SearchRepositoriesUseCase by inject()
+    private lateinit var listAdapter: SearchAdapter
+    private lateinit var viewDataBinding: SearchFragmentBinding
+
+    private val _items = MutableLiveData<List<Repository>>().apply { value = emptyList() }
+    val items: LiveData<List<Repository>> = _items
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+        viewDataBinding =
+            SearchFragmentBinding.inflate(inflater, container, false).apply {
+                fragment = this@SearchFragment
+            }
+        return viewDataBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        searchRepositoryByKeyWord("myung6024")
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        setupListAdapter()
     }
 
-    private fun searchRepositoryByKeyWord(searchKeyWord: String) {
+    private fun setupListAdapter() {
+        listAdapter = SearchAdapter(this)
+        viewDataBinding.rvRepository.adapter = listAdapter
+    }
+
+    val searchRepositoryByKeyWord = fun(searchKeyWord: String) {
         CoroutineScope(Dispatchers.Main).launch {
             val result = searchRepositoriesUseCase(searchKeyWord)
             if (result is Success) {
+                _items.value = result.data.repositories
                 Log.d("loloss", result.data.toString())
             } else {
                 Log.d("loloss", "fail")
