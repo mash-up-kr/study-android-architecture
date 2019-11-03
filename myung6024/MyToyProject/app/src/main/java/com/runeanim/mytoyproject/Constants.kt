5c74a470efe6d007537f9911a4e3250a706702f3
myung6024 <myung6024@gmail.com>
2019-11-03 16:59:27 +0900

    (#1) 의미없는 주석 삭제

--- myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/Constants.kt
+++ myung6024/MyToyProject/app/src/main/java/com/runeanim/mytoyproject/Constants.kt
@@ -1,6 +1,6 @@
 package com.runeanim.mytoyproject
 
 object Constants {
-    const val EXTRA_USER_NAME: String = "extra_user_name"  // 1초
+    const val EXTRA_USER_NAME: String = "extra_user_name"
     const val EXTRA_REPOSITORY_URL: String = "extra_repo_url"
 }
