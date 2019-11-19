4b09fca6974dc8e8b4e56da629a3497740f7acf0
godjoy <tlschgml556@gmail.com>
2019-11-19 16:16:55 +0900

    [추가] Repo에도 네트워크 관련 에러 처리

--- godjoy/app/src/main/java/com/example/myapplication/ui/RepoActivity.kt
+++ godjoy/app/src/main/java/com/example/myapplication/ui/RepoActivity.kt
@@ -2,17 +2,18 @@ package com.example.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.myapplication.Network.model.SearchRepo
 import com.example.myapplication.Network.SearchRetrofit
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.activity_repo.*
-import kotlinx.android.synthetic.main.activity_search.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class RepoActivity : AppCompatActivity() {
+class RepoActivity : AppCompatActivity(), NetworkException {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +24,17 @@ class RepoActivity : AppCompatActivity() {
     }
 
     fun getRepoResponse(userName : String, repoName : String){
-        SearchActivity.showProgress(pbActivityRepo)
+       showProgress(pbActivityRepo)
 
         val getRepoResponse = SearchRetrofit.getService().getRepoResponse(userName, repoName)
         getRepoResponse.enqueue(object : Callback<SearchRepo>{
             override fun onFailure(call: Call<SearchRepo>, t: Throwable) {
+                showError(tvNoResultActivityRepo)
             }
 
             override fun onResponse(call: Call<SearchRepo>, response: Response<SearchRepo>) {
                 if(response.isSuccessful){
-                    SearchActivity.hideProgress(pbActivityRepo)
+                    hideProgress(pbActivityRepo)
 
                     response.body().let {
                         Glide
@@ -58,9 +60,17 @@ class RepoActivity : AppCompatActivity() {
                         tvUpdateTimeRepoActivity.text = it.updateTime.subSequence(11,19)
                     }
                 }
+                else{
+                    hideProgress(pbActivityRepo)
+                    showError(tvNoResultActivityRepo)
+                }
             }
         })
     }
 
+    override fun showError(tv: TextView) {
+        llRepoActivity.visibility = View.GONE
+        super.showError(tv)
+    }
 
 }
